c87abb9fb0ce5ad8c06e07eeeffbd016d43b8eea
Dawson Lu <dawson.lu@stryten.com>
2024-04-16 16:02:39 -0400

    Added solution to playback.py

---
+++ Problem Set 0/playback.py
@@ -0,0 +1,16 @@
+"""
+Playback Speed
+
+In a file called playback.py, implement a program in Python that prompts the
+user for input and then outputs that same input, replacing each space with ...
+(i.e., three periods).
+"""
+
+
+# Function to replace space with '...'T
+def replaceSpace(input):
+    return input.replace(' ', '...')
+
+
+if __name__ == "__main__":
+    print(replaceSpace(input()))
