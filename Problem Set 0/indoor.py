ee1fe5e98620a5d73e1a0edd616cdc20d064523f
Dawson Lu <dawson.lu@stryten.com>
2024-04-16 15:57:04 -0400

    Added indoor.py to problem set 0

---
+++ Problem Set 0/indoor.py
@@ -0,0 +1,18 @@
+"""
+Indoor Voice
+
+In a file called indoor.py, implement a program in Python that prompts the user
+for input and then outputs that same input in lowercase. Punctuation and
+whitespace should be outputted unchanged. You're welcome, but not required, to
+prompt the user explicitly, as by passing a str of your own as an argument to
+input.
+"""
+
+
+# Function to convert the input to lowercase
+def indoor(input):
+    return input.lower()
+
+
+if __name__ == "__main__":
+    print(indoor(input()))
