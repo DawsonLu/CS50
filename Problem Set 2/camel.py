d89fe2daaf60189cd6f87f868beb2bf12524fa5e
Dawson Lu <dawson.lu@stryten.com>
2024-05-14 10:54:04 -0400

    Added camel.py file, problem description and solution

---
+++ Problem Set 2/camel.py
@@ -0,0 +1,18 @@
+"""
+In a file called camel.py, implement a program that prompts the user for the
+name of a variable in camel case and outputs the corresponding name in snake
+case. Assume that the user’s input will indeed be in camel case.
+"""
+
+if __name__ == "__main__":
+    get_input = input("camelCase: ").strip()
+    char_list = []
+    
+    for c in get_input:
+        if c.isupper():
+            char_list.append(f"_{c.lower()}")
+        
+        else:
+            char_list.append(c)
+            
+    print(''.join(char_list))
